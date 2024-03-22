33695471e5c9f3a28ec6428ba1f60fe2c5f467b7
kash <iam8139@gmail.com>
2024-03-23 01:32:18 +0530

    Merge two sorted linked list

---
+++ MergeSortedList.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MergeSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head = new ListNode();
+        ListNode temp = head;
+
+        while (list1 != null && list2!= null) {
+            if (list1.val <= list2.val) {
+                head.next = list1;
+                list1 = list1.next;
+            } else {
+                head.next = list2;
+                list2 = list2.next;
+            }
+            head = head.next;
+        }
+
+        head.next = list1 != null ? list1 : list2;
+        return temp.next;
+    }
+}
