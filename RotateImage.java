fd96f4f28079bfea39dd447e2c8d96c2cdfdeecd
kash <iam8139@gmail.com>
2024-05-17 22:50:32 +0530

    Rotate image by 90 degrees to the right

---
+++ RotateImage.java
@@ -0,0 +1,18 @@
+class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] temp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[i][j] = matrix[n-1-j][i];
+                System.out.println(temp[i][j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = temp[i][j];
+            }
+        }
+    }
+}
